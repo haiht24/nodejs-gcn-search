d1f2df7464fb20603bce0f77c3dcf4e463b893ab
HaiHT <haiht369@gmail.com>
2018-07-29 17:38:12 +0700

    Get results from couponmarathon + CouponChecked

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -8,11 +8,10 @@ function findIn(url) {
         password = "matkhau",
         auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
     return new Promise(resolve => {
-        request( {url, headers : { "Authorization" : auth }} ,  function(error, response, html){
+        // request( {url, headers : { "Authorization" : auth }} ,  function(error, response, html){
+        request( url ,  function(error, response, html){
             if(!error){
                 var $ = cheerio.load(html);
-                if (!error && response.statusCode == 200) {
-                }
                 resolve ($('body').text());
             }
             resolve (error);
@@ -28,18 +27,9 @@ module.exports = function (app, db) {
             var keyword = req.body.keyword;
             keyword = keyword.replace(/ /g,'-').toLowerCase();
             arrFunctions.push(findIn('http://couponmarathon.com/searchFromApi?keyword=' + keyword));
-            arrFunctions.push(findIn('http://allyourcoupons.com/searchFromApi?keyword=' + keyword));
+            arrFunctions.push(findIn('http://CouponChecked.com/searchFromApi?keyword=' + keyword));
             Promise.all(arrFunctions).then(function (results) {
-                if(results.length > 1){
-                    var merged=[];
-                    for(var i = 0; i < results.length; i++){
-                        var r = results[i];
-                        merged = merged.concat(r);
-                    }
-                }
-                console.log('test service auto build jenkin');//asdasdasd
-                res.send(merged);
-                console.log(merged);
+                res.send(results);
             }).catch(function(error){
                 res.send(error);
                 console.log('error 1');
